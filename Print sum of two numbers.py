cc2c270c10fd2590274efc9ebb8ea634cd346c5a
Avila-Princy-M01 <avila.m@s.amity.edu>
2025-03-22 12:35:20 +0530

    Added files

---
+++ Print sum of two numbers.py
@@ -0,0 +1,5 @@
+# Write a program that prints the sum of two numbers
+# 2495 and 789358
+
+print(2495 + 789358)
+# Expected output: 791853
---
+++ Subtraction of two numbers.py
@@ -0,0 +1,5 @@
+# Write a program that prints the result when 
+# 596 is subtracted from 193856
+
+print(193856 - 596)
+# Output: 193260
